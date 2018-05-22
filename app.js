46bd77848793bafe4be0813c5540f18a29fe338a
Ganoganano <>
2018-05-22 15:57:06 +0900

    Chap12 starting to creat create TODO app

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-
-
 (function() {
 
+    let Task = Backbone.Model.extend({})
+    let Tasks = Backbone.Collection.extend({model: Task})
 
+    let TaskView = Backbone.View.extend({})
+    let TasksView = Backbone.View.extend({})
+
+    let tasks = new Tasks([
+        {
+            title: 'task1',
+            complted: true
+        },
+        {
+            title: 'task2'
+        },
+        {
+            title: 'task3'
+        }
+    ])
 })();
