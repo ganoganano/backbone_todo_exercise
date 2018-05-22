9cc6f027ef81f8fbe8d1946371937dc76da0e943
Ganoganano <>
2018-05-22 18:20:36 +0900

    Chap15 Function to delete Task

--- app.js
+++ app.js
@@ -10,14 +10,37 @@
 
     let TaskView = Backbone.View.extend({
         tagName: 'li',
+        initialize: function(){
+            this.model.on('destroy', this.remove, this)
+        },
+        events: {
+            "click .delete": "destroy"
+        },
+        destroy: function(){
+            if(confirm('are you sure?')){
+                this.model.destroy()
+            }
+        },
+        remove: function(){
+            this.$el.remove();
+        },
         tempalte: _.template( $('#task-template').html() ),
         render: function() {
-            let template = this.tempalte(this,model.toJSON())
+            let template = this.tempalte(this.model.toJSON())
             this.$el.html(template)
             return this
         }
     })
-    let TasksView = Backbone.View.extend({})
+    let TasksView = Backbone.View.extend({
+        tagName: 'ul',
+        render: function(){
+            this.collection.each((task)=>{
+            let taskView = new TaskView({model: task})
+            this.$el.append(taskView.render().el)
+            })
+            return this
+        }
+    })
 
     let tasks = new Tasks([
         {
@@ -31,4 +54,7 @@
             title: 'task3'
         }
     ])
+
+    let tasksView = new TasksView({collection: tasks})
+    $('#tasks').html(tasksView.render().el)
 })();
