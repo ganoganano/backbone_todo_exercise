fbf36261d052ec0ce406e81e51a299d91c73383a
Ganoganano <>
2018-05-22 15:24:06 +0900

    Chap2 create Model on Backbone

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 
 (function() {
 
+//Model
+
+let Task = Backbone.Model.extend({
+    defaults: {
+        title : "test title",
+        completed: false
+    }
+});
+
+let task = new Task();
 
 
 })();
