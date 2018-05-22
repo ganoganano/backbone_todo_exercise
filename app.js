0a0bb19add96d30d1c6f8a18bd692d14dde5ab52
Ganoganano <>
2018-05-22 15:27:14 +0900

    chap5 Using Validate and Methods on Model

--- app.js
+++ app.js
@@ -6,14 +6,27 @@
 
 let Task = Backbone.Model.extend({
     defaults: {
-        title : "test title",
+        title : "title",
         completed: false
+    },
+    validate: function(attr) {
+        if(_.isEmpty(attr.title)) {
+            return "title must not be empty."
+        }
+    },
+    toggle: function() {
+        this.set('completed', !this.get('completed'))
     }
 });
 
-let task = new Task();
+let task1 = new Task();
 
-task.set('title', 'changed title')
-console.log(task.get('title'));
+console.log(task1.toJSON());
+task1.toggle();
+console.log(task1.toJSON());
+
+task1.set({title: ''}, {validate: true})
+
+console.log(task1.toJSON());
 
 })();
