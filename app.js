1fa810472f1e8f4d1751a9a5d63ee1e6701d2a5d
Ganoganano <>
2018-05-22 16:01:11 +0900

    Chap13 Model and View for Task

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 (function() {
 
-    let Task = Backbone.Model.extend({})
+    let Task = Backbone.Model.extend({
+        defaults:{
+            title: 'something',
+            completed: false
+        }
+    })
     let Tasks = Backbone.Collection.extend({model: Task})
 
-    let TaskView = Backbone.View.extend({})
+    let TaskView = Backbone.View.extend({
+        tagName: 'li',
+        tempalte: _.template( $('#task-template').html() ),
+        render: function() {
+            let template = this.tempalte(this,model.toJSON())
+            this.$el.html(template)
+            return this
+        }
+    })
     let TasksView = Backbone.View.extend({})
 
     let tasks = new Tasks([
